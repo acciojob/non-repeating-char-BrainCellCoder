f686574dc6484f30849cb9d64d7a43c5a7202840
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-13 22:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const countObj={};
+
+	for(let char of str){
+		if(countObj.hasOwnProperty(char)){
+			countObj[char]++;
+		}else{
+			countObj[char] = 1;
+		}
+	}
+
+	for(let char of str){
+		if(countObj[char] == 1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
+
+
